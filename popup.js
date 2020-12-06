01bcbe08a7e16c3e05d738176a4b53249f04aa79
Dhananjay Choudhary <40604207+DHANANJAY130@users.noreply.github.com>
2020-12-06 14:22:36 +0000

    removed unnecessary code

--- popup.js
+++ popup.js
@@ -1,8 +1,5 @@
 $(function(){
-$("#sbmt").click(function(){
-    $("h1").text("Hey There!")
-    // $("#url1").val("this is url")
-});
+
 chrome.tabs.getSelected(null,function(tab) {
 
     $("#T1").val(tab.title)
